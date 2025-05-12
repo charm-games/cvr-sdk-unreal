8e15edccaa266745d30de67e4ffe47006f62ac6d
Dinos Tsiknis <dinos.tsiknis@charmgames.com>
2025-05-12 15:10:47 -0700

    Fixed compilation errors. UNCORE-960

--- Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Private/C3DComponents/EnhancedInputTracker.cpp
+++ Cognitive3DTest/Plugins/Cognitive3D/Source/Cognitive3D/Private/C3DComponents/EnhancedInputTracker.cpp
@@ -3,6 +3,10 @@
 
 #include "C3DComponents/EnhancedInputTracker.h"
 
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/PlayerController.h"
+
+
 // Sets default values for this component's properties
 UEnhancedInputTracker::UEnhancedInputTracker()
 {
